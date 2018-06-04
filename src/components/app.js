836813df5350034dcbb90f26990afde79a941005
dipsywong98 <facebookarmyoffcal@gmail.com>
2018-06-04 18:38:58 +0800

    added google analytics

--- src/components/app.js
+++ src/components/app.js
@@ -1,8 +1,16 @@
 import { Component } from 'react'
 import { Provider } from 'react-redux'
 import { store } from '../lib/store'
+import { initGA, logPageView } from '../lib/ga'
 
 export default class App extends Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+    }
+    logPageView()
+  }
   render() {
     return <Provider store={store} children={this.props.children} />
   }
