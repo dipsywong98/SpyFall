11633843d46c17839c3107d618e9f954fef4a117
dipsywong98 <ycwongal@connect.ust.hk>
2020-01-24 14:20:08 +0800

    Updates

---
+++ precache-manifest.8854a5e415a1c8323f700dc755d96039.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af5ee1994f68f2c7db7e2b8f065d28f7",
+    "url": "/SpyFall/index.html"
+  },
+  {
+    "revision": "8abaacefddb9696a905b",
+    "url": "/SpyFall/static/js/2.914608f2.chunk.js"
+  },
+  {
+    "revision": "11b85fdfd2d9a9bba538c2dd62bfeab3",
+    "url": "/SpyFall/static/js/2.914608f2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c41cd757f122ec83e781",
+    "url": "/SpyFall/static/js/main.477c3d70.chunk.js"
+  },
+  {
+    "revision": "53254ed9856539d357fe",
+    "url": "/SpyFall/static/js/runtime-main.c6d5147d.js"
+  }
+]);
